6f9863e7beb863282ecd138a7b0b0a1b0cb86a13
janardhana sai krishna <45102237+janardhan5031@users.noreply.github.com>
2022-05-24 15:42:49 +0530

    2nd font green& odd elements background green

--- dom.js
+++ dom.js
@@ -3,20 +3,25 @@
 //var header=document.getElementById('header-title');
 //header.style.color='blue';
 //console.log(header);
-
-//getElementsByClassName
+//
 var item=document.getElementsByClassName('list-group-item');
 //console.log(item);
 //item[1].textContent='Hello';
 //item[1].style.fontWeight='bold';
 //item[1].style.color='green';
 //
-item[2].style.backgroundColor='green';
-item[0].style.fontWeight='bold';
-item[1].style.fontWeight='bold';
-item[2].style.fontWeight='bold';
-item[3].style.fontWeight='bold';
+//item[2].style.backgroundColor='green';
+item[1].style.backgroundColor='#90EE90';
+for(var x of item){
+    x.style.fontWeight='bold';
+}
+item[2].style.display='none';
+item[1].style.color='green';
 
-//getElementsByTagName
-var ite=document.getElementsByTagName('li');
-ite[4].textContent='hello jani';
+//var ite=document.getElementsByTagName('li');
+//ite[4].textContent='hello jani';
+
+var odd=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+}
