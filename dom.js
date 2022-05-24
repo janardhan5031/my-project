b704b530e798c63fabc68305f918d3c198659aad
janardhana sai krishna <45102237+janardhan5031@users.noreply.github.com>
2022-05-24 11:06:41 +0530

    dom file created

---
+++ dom.js
@@ -0,0 +1,11 @@
+//console.dir(document)
+////console.log(document.getElementById('header-title'))
+//var header=document.getElementById('header-title');
+//header.style.color='blue';
+//console.log(header);
+//
+var item=document.getElementsByClassName('list-group-item');
+console.log(item);
+item[1].textContent='Hello';
+item[1].style.fontWeight='bold';
+item[1].style.color='green';
